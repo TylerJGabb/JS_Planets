df30372e3e470e46f37f2acc8e6cbd69465dbaf1
tylerjgabb <tylerjgabb@gmail.com>
2018-01-14 05:48:01 -0500

    proof of concept done, perhaps add 3d orbits

--- js/introduction.js
+++ js/introduction.js
@@ -1,5 +1,7 @@
 //creating the scene
 
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
@@ -9,22 +11,56 @@ document.body.appendChild( renderer.domElement );
 
 
 //add the cube
-var geometry = new THREE.SphereGeometry(1,10,10);
+var geometry = new THREE.SphereGeometry(2,10,10);
 var material = new THREE.MeshBasicMaterial({
-    color : 0x00ff00,
+    color : 0xff0000,
     wireframe : true
 });
 var sphere = new THREE.Mesh(geometry,material);
+
+var material2 = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    wireframe: true
+});
+
+var geometry2 = new THREE.SphereGeometry(1,10,10);
+var orbiter = new THREE.Mesh(geometry2,material2);
+
+
+
+var controls = new THREE.OrbitControls( camera )
+camera.position.set(0,20,100);
+controls.update();
+
+
+
+
+
 scene.add(sphere);
+scene.add(orbiter);
 
-camera.position.z = 10;
-console.log(sphere.position);
-
-
+var theta = 0;
+var theta_orbiter = 0;
+var interval_sphere = 2*Math.PI/240;
+var interval_orbiter = 2*Math.PI/90;
+//tmax = 2pi; 
 function animate() {
     requestAnimationFrame( animate );
-    sphere.rotation.x += 1/120;
-    sphere.rotation.y += 1/120;
+    sphere.position.x = 20*Math.cos(theta);
+    sphere.position.y = 20*Math.sin(theta);
+
+    orbiter.position.x = sphere.position.x + 4*Math.cos(theta_orbiter);
+    orbiter.position.y = sphere.position.y + 4*Math.sin(theta_orbiter);
+    theta += interval_sphere;
+    theta_orbiter += interval_orbiter;
+    if(theta > 2*Math.PI){
+        theta = 0;
+    }
+
+    if(theta_orbiter > 2*Math.PI){
+        theta_orbiter = 0;
+    }
+    controls.update();
     renderer.render( scene, camera );
 }
 animate();
